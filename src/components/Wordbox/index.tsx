7faf2f33198fa15779f055e0857e0f6af38f6339
Paige <pa.krajcova@gmail.com>
2025-11-04 19:37:21 +0100

    Dodelane cviceni 1 - datluj

--- src/components/Wordbox/index.tsx
+++ src/components/Wordbox/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.css';
 
 interface IWordboxProp {
@@ -6,11 +6,37 @@ interface IWordboxProp {
 }
 
 const Wordbox : React.FC<IWordboxProp> = ({ word }) => {
-  const [lettersLeft] = useState<string>(word);  
+  const [lettersLeft, setLettersLeft] = useState<string>(word);  
   
+  useEffect(
+    () => {
+      const handleKeyUp = (e: KeyboardEvent) => {
+        const key = e.key;
+        //console.log(e.key.toUpperCase())
+      
+
+        if (key === lettersLeft[0]) {
+          console.log("spravně")
+          //setLettersLeft(lettersLeft.slice(1)) //chci od prvního znaku až do konce 0,1 ...
+          setLettersLeft(x => x.slice(1))
+          
+        }
+      }
+
+      document.addEventListener("keyup", handleKeyUp)
+      
+
+      return() => {
+        document.removeEventListener("keyup", handleKeyUp)
+      }
+    }
+
+   
+  )
   return (
     <div className="wordbox">{lettersLeft}</div>
   );
+  [lettersLeft];
 };
 
 export default Wordbox;
