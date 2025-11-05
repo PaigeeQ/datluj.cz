c9c7decb83fed18c5b1292b9cbb9aff79b9c8992
Paige <pa.krajcova@gmail.com>
2025-11-05 20:46:58 +0100

    Úkol č. 3 splněn

--- src/components/Wordbox/index.tsx
+++ src/components/Wordbox/index.tsx
@@ -8,39 +8,51 @@ interface IWordboxProp {
 
 const Wordbox : React.FC<IWordboxProp> = ({ word, onFinish }) => {
   const [lettersLeft, setLettersLeft] = useState<string>(word);  
+  const [mistake, setMistake] = useState<boolean>(false);
   
-  useEffect(
-    () => {
+  useEffect(() => {
+    setLettersLeft(word);
+    setMistake(false);
+  }, [word]);
+  
+  useEffect(() => {
       const handleKeyUp = (e: KeyboardEvent) => {
         const key = e.key;
         //console.log(e.key.toUpperCase())
       
-
         if (key === lettersLeft[0]) {
+          setMistake(false);
           //console.log("spravně")
             if(lettersLeft.length === 1) {
               onFinish();
             }
-
+            else {
           //setLettersLeft(lettersLeft.slice(1)) //chci od prvního znaku až do konce 0,1 ...
-          setLettersLeft(x => x.slice(1))
+          setLettersLeft(x => x.slice(1));
+            }
           
-        }
+        } else {
+        setMistake(true);
+        setTimeout(() => setMistake(false), 300); 
       }
-
+    };
+    
       document.addEventListener("keyup", handleKeyUp)
-      
-
       return() => {
         document.removeEventListener("keyup", handleKeyUp)
-      }
-    }
-    
-  )
+      };
+    },
+[lettersLeft, onFinish]);
+
+    const wordboxClass = mistake ? 'wordbox mistake' : 'wordbox';
+
   return (
-    <div className="wordbox">{lettersLeft}</div>
+  <div className={wordboxClass}>{lettersLeft}</div>
   );
-  [lettersLeft];
+
 };
 
 export default Wordbox;
+
+
+//Tady nám stale state nehrozí. Mistake jen přenastavuju, nikde ji nečtu, takže se mi nemá co „zaseknout“.
