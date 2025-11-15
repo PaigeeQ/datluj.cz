2dcf928075bd4077b8035e60362a50f8f2c464dc
Paige <pa.krajcova@gmail.com>
2025-11-15 19:29:32 +0100

    Přidana uvodní stranka - starscreen a button, doladěné css

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import Stage from './components/Stage';
+import { useState } from 'react';
 
 const App: React.FC = () => {
+
+  const [started, setStarted] = useState(false);
+if (!started) {
+  return (
+  <div className="start-screen">
+        <h1>DATLUJ.cz</h1>
+        <p>Stiskni Start a pojď na to! </p>
+        <button className="start-btn" onClick={() => setStarted(true)}>
+          Start
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div className="container">
-      <h1>Datlování</h1>
+      <h1>Datlování aneb Datluj.cz</h1>
       <Stage />
     </div>
   );
